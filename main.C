9f21a5b07c9863f556010146c73afb2c58bad39b
aruan4 <aruan4@uwo.ca>
2024-03-31 22:28:16 -0400

    first display commit

---
+++ main.C
@@ -0,0 +1,37 @@
+#define HEX3_HEX0_BASE        0xFF200020
+	
+//pointers
+volatile unsigned int *hex_ptr = (unsigned int *)HEX3_HEX0_BASE;
+volatile unsigned int *hex1_ptr = (unsigned int *)HEX3_HEX0_BASE;
+volatile unsigned int *hex2_ptr = (unsigned int *)HEX3_HEX0_BASE;
+volatile unsigned int *hex3_ptr = (unsigned int *)HEX3_HEX0_BASE;
+
+//hex numbers
+const int hexDigits[10] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F  // 9
+};
+
+//display hex
+void DisplayHex(int value){
+}
+
+//get hex
+int getHex(int value){
+	return hexDigits[value];
+}
+
+//main
+int main(void){
+	*hex_ptr = (hexDigits[1] << 16) |
+			  	  (hexDigits[2] << 8) | 
+			       hexDigits[0];
+}
